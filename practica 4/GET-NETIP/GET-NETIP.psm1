33e86aa9e5f0e15a8f49cc3f18b4d5b2113c6046
davidnm123 <64614576+davidnm123@users.noreply.github.com>
2020-10-15 15:05:22 -0500

    Delete GET-NETIP.psm1

--- practica 4/GET-NETIP/GET-NETIP.psm1
+++
@@ -1,8 +0,0 @@
-﻿
-function GET-NETIP{
-    $variable = Get-NetIPConfiguration
-    $IP = $variable.IPv4Address[1].IPAddress
-    $puerta = $variable.IPv4DefaultGateway.NextHOP
-    Write-Host "LA IP DEL INTERNET ES:  $IP" 
-    Write-Host "LA PUERTA DE CONEXIÓN DE INTERNET ES: $puerta" 
-}
