86dbc2537ea7f1c08b418241c297554545a13e6f
davidnm123 <64614576+davidnm123@users.noreply.github.com>
2020-10-15 15:02:30 -0500

    Delete Ver-StatusPerfil.psm1

--- practica 4/Ver-StatusPerfil/Ver-StatusPerfil.psm1
+++
@@ -1,9 +0,0 @@
-﻿function Ver-StatusPerfil{ 
-	param([Parameter(Mandatory)] [ValidateSet("Public","Private")] [string] $perfil) 
-	$status = Get-NetFirewallProfile -Name $perfil Write-Host "Perfil:" $perfil 
-	if($status.enabled){ 
-		Write-Host "Status: Activado" 
-	} else{ 
-		Write-Host "Status: Desactivado" 
-	} 
-} 
